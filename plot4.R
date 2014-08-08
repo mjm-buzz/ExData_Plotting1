d56d6479870296b2792062e597e943ce8a8cb213
Michael <mjmbuzz@gmail.com>
2014-08-07 20:54:24 -0400

    updated solutions

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@
 # Data Source: http://archive.ics.uci.edu/ml/
 # Data download: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
-# Read data for dates 02/01/2007 to 02/02/2001
+# Read data for dates 02/01/2007 to 02/02/2007
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";",
                    skip = 66636, nrows = 2880)
 names(data) <- c("Date", "Time", "Global_active_power", 
